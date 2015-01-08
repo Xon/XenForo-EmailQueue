c0a90e8c1f3863886397103134dea51aa6c4f0bf
Xon <tazipper1@hotmail.com>
2015-01-08 20:19:56 +0800

    Cache model in email sending path

--- upload/library/SV/EmailQueue/XenForo/Mail.php
+++ upload/library/SV/EmailQueue/XenForo/Mail.php
@@ -10,11 +10,22 @@ class SV_EmailQueue_XenForo_Mail extends XFCP_SV_EmailQueue_XenForo_Mail
         }
         if (XenForo_Application::get('config')->enableMailQueue)
         {
-            return XenForo_Model::create('XenForo_Model_MailQueue')->insertMailQueue($mailObj);
+            return $this->_getMailQueueModel()->insertMailQueue($mailObj);
         }
         else
         {
             return parent::sendMail($mailObj);
         }
+    }
+
+    static $_mailQueue = null;
+
+    protected function _getMailQueueModel()
+    {
+        if (self::$_mailQueue === null)
+        {
+            self::$_mailQueue = XenForo_Model::create("XenForo_Model_MailQueue");
+        }
+        return self::$_mailQueue;
     }
 }
