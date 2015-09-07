b9c20a827987c2ea6d4de3554d5ab513c7acf0f5
Xon <tazipper1@hotmail.com>
2015-09-07 09:08:27 +0800

    Support for queuing XF 1.5 bulk emails on failure.

---
+++ upload/library/SV/EmailQueue/Transport/FailedDefer.php
@@ -0,0 +1,40 @@
+<?php
+
+class SV_EmailQueue_Transport_FailedDefer extends Zend_Mail_Transport_Abstract
+{
+    protected $_mailQueue = null;
+    protected $transport = null;
+
+    public function setWrappedTransport(Zend_Mail_Transport_Abstract $transport)
+    {
+        $this->transport =  $transport;
+    }
+
+    public function send(Zend_Mail $mail)
+    {
+        try
+        {
+            $this->transport->send($mail);
+        }
+        catch (Exception $e)
+        {
+            $toEmails = implode(', ', $mail->getRecipients());
+            XenForo_Error::logException($e, false, "Email to $toEmails failed: ");
+            $this->_getMailQueueModel()->insertFailedMailQueue($mailObj);
+        }
+    }
+
+    protected function _sendMail()
+    {
+        throw new Exception("This email transport doesn't actually send email");
+    }
+
+    protected function _getMailQueueModel()
+    {
+        if ($this->_mailQueue === null)
+        {
+            $this->_mailQueue = XenForo_Model::create("XenForo_Model_MailQueue");
+        }
+        return $this->_mailQueue;
+    }
+}
---
+++ upload/library/SV/EmailQueue/XenForo/Deferred/UserEmail.php
@@ -0,0 +1,27 @@
+<?php
+
+class SV_EmailQueue_XenForo_Deferred_UserEmail extends XFCP_SV_EmailQueue_XenForo_Deferred_UserEmail
+{
+    protected $_deferMailTransport = null;
+
+    protected function _getDeferMailTransport(Zend_Mail_Transport_Abstract $transport)
+    {
+        if($this->_deferMailTransport === null)
+        {
+            $class = XenForo_Application::resolveDynamicClass("SV_EmailQueue_Transport_FailedDefer");
+            $this->_deferMailTransport = new $class();
+            $this->setWrappedTransport($transport);
+        }
+        return $this->_deferMailTransport;
+    }
+
+    protected function _sendEmail(array $email, array $user, Zend_Mail_Transport_Abstract $transport)
+    {
+        if (XenForo_Application::get('config')->enableMailQueue)
+        {
+            // use a tiny custom Mail transport which just posts to XenForo_Mail::insertFailedMailQueue on failure
+            $transport = $this->_getDeferMailTransport($transport);
+        }
+        return parent::_sendEmail($email, $user, $transport);
+    }
+}
